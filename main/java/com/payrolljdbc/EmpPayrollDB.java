26aa5aa82fbcf89f1226e85ef1556e8b1327fd73
Nishi Gautam <nishigautam.nishu2@gmail.com>
2021-05-21 19:56:09 +0530

    [REFACTOR] : Connecting to database

--- main/java/com/payrolljdbc/EmpPayrollDB.java
+++ main/java/com/payrolljdbc/EmpPayrollDB.java
@@ -3,19 +3,20 @@ package com.payrolljdbc;
 import java.sql.Connection;
 import java.sql.Driver;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Enumeration;
 
 public class EmpPayrollDB {
-    public static void main(String[] args) {
-        String jdbcURL = "jdbc:mysql:// localhost:3306/employee_payroll";
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        String jdbcURL = "jdbc:mysql://localhost:3306/payroll_service";
         String userName = "root";
-        String password = "nishi@123";
+        String password = "root";
         Connection connection;
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             System.out.println("Driver loaded..");
         } catch (ClassNotFoundException e) {
-            throw new IllegalStateException("Cannot find the driver in the classpath!" , e);
+            throw new IllegalStateException("Cannot find the driver in the classpath!", e);
         }
 
         listDrivers();
@@ -24,15 +25,15 @@ public class EmpPayrollDB {
             System.out.println("Connecting to database :" + jdbcURL);
             connection = DriverManager.getConnection(jdbcURL,userName,password);
             System.out.println("Connection is successful!" + connection);
-        } catch (Exception e) {
+          } catch (SQLException e) {
             e.printStackTrace();
-        }
+          }
     }
 
     private static void listDrivers() {
         Enumeration<Driver> driverList = DriverManager.getDrivers();
         while (driverList.hasMoreElements()) {
-            Driver driverClass = (Driver) driverList.nextElement();
+            Driver driverClass = driverList.nextElement();
             System.out.println("Driver:" + driverClass.getClass().getName());
         }
     }
